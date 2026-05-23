843f535c202221fb7faf456f897419a8b8fc029f
Suhaim Khalid <suhaimkhalid007@gmail.com>
2026-05-23 20:19:36 +0100

    add navbar animation

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,9 +1,33 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useRef } from "react";
+
 export const Header = () => {
+  const nav_ani = useRef(null);
+
+  useGSAP(
+    () => {
+      const t1 = gsap.timeline();
+      t1.from(".navbar_h1", {
+        y: -50,
+        opacity: 0,
+        duration: 1,
+      });
+      t1.from(".nav-item", {
+        y: -50,
+        opacity: 0,
+        duration: 1.5,
+        stagger: 0.3,
+      });
+    },
+    { scope: nav_ani },
+  );
+
   return (
     <>
       <header>
         <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-          <div className="container">
+          <div className="container" ref={nav_ani}>
             <h1 className="navbar_h1">Portfolio</h1>
 
             <button
@@ -41,6 +65,11 @@ export const Header = () => {
                 <li className="nav-item">
                   <a href="#skills" className="nav-link">
                     Skills
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#recommendations">
+                    Recommendations
                   </a>
                 </li>
                 <li className="nav-item">
